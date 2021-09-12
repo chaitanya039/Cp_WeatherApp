52dce06585bff0ab6d3d607e0526bfed9793e384
chaitanya pansare <chaitanyapansare33607@gmail.com>
2021-09-12 11:16:41 +0530

    debugging

--- public/js/weatherForecastcity2.js
+++ public/js/weatherForecastcity2.js
@@ -16,22 +16,23 @@ const getWeatherData = async () => {
             const response = await fetch(url);
             const data = await response.json();
             console.log(data);
-            // const latitude = data.coord.lat;
-            // const longitude = data.coord.lon;
+            const latitude = data.coord.lat;
+            const longitude = data.coord.lon;
             // countryEl.innerHTML = `<div class="country" id="country"><span style="padding: 0.5rem; border: 0.5px solid white; border-radius:0.5rem;box-shadow: -2px 2px 3px -0.5px white;">${data.name}, ${data.sys.country}</span>&nbsp;&nbsp;${data.coord.lat}N&nbsp;&nbsp;&nbsp;${data.coord.lon}E </div>`
 
-            // await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=6e2063591e0fcbed66fc99e142be55ab`)
-            //     .then(response => response.json())
-            //     .then(data => {
-            //         console.log(data);
-            //         showWhetherData(data);
-            //     })
+            await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=6e2063591e0fcbed66fc99e142be55ab`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    // showWhetherData(data);
+                })
 
-            // await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=daily&units=metric&appid=6e2063591e0fcbed66fc99e142be55ab`)
-            //     .then(response => response.json())
-            //     .then(data => {
-            //         showHourData(data);
-            //     });
+            await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=daily&units=metric&appid=6e2063591e0fcbed66fc99e142be55ab`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    // showHourData(data);
+                });
 
             // display_container.style.display = "block";
             // search_container.style.display = "none";
